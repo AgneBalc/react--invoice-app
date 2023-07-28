3545acc6b7fc20612811f45860a2b3f5c69ae1f4
AgneBalc <agne.martinaityte@gmail.com>
2023-07-28 10:43:00 +0300

    InvoicesList component created

---
+++ src/components/invoices/InvoicesList.tsx
@@ -0,0 +1,17 @@
+// const InvoicesList = () => {
+//   return (
+//     <ul>
+//       <li>
+//         <a href="">
+//           <h3>{invoices.id}</h3>
+//           <span>{invoices.paymentDue}</span>
+//           <span>{invoices.clientName}</span>
+//           <span>&euro;{invoices.total}</span>
+//           <div>{invoices.status}</div>
+//         </a>
+//       </li>
+//     </ul>
+//   );
+// };
+
+// export default InvoicesList;
