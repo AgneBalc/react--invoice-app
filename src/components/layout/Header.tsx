03a595dfa7eda8091f9a07ef09236e820b449c8e
AgneBalc <agne.martinaityte@gmail.com>
2023-07-28 10:30:12 +0300

    Header content and styles created

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,28 @@
+import StyledHeader from "../../styles/header-styles";
+import Logo from "../../assets/logo.svg";
+import { ReactComponent as IconMoon } from "../../assets/icon-moon.svg";
+import Avatar from "../../assets/image-avatar.jpg";
+
 const Header = () => {
-  return <header></header>;
+  return (
+    <StyledHeader>
+      <img src={Logo} alt="Invoice App logo" />
+      <nav>
+        <ul className="actionBar">
+          <li className="themeIcon">
+            <a href="">
+              <IconMoon />
+            </a>
+          </li>
+          <li className="avatarIcon">
+            <a href="">
+              <img src={Avatar} alt="avatar image" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </StyledHeader>
+  );
 };
 
 export default Header;
