2b9cceef79b5efe4999d36f552ea55a61679589f
AgneBalc <agne.martinaityte@gmail.com>
2023-07-28 11:58:23 +0300

    fetchInvoices function and Invoices component content created

--- src/components/invoices/InvoicesList.tsx
+++ src/components/invoices/InvoicesList.tsx
@@ -1,17 +1,21 @@
-// const InvoicesList = () => {
-//   return (
-//     <ul>
-//       <li>
-//         <a href="">
-//           <h3>{invoices.id}</h3>
-//           <span>{invoices.paymentDue}</span>
-//           <span>{invoices.clientName}</span>
-//           <span>&euro;{invoices.total}</span>
-//           <div>{invoices.status}</div>
-//         </a>
-//       </li>
-//     </ul>
-//   );
-// };
+import { Invoice } from "../../types";
 
-// export default InvoicesList;
+type InvoiceProps = { invoice: Invoice };
+
+const InvoicesList = ({ invoice }: InvoiceProps) => {
+  return (
+    <ul>
+      <li>
+        <a href="">
+          <h3>{invoice.id}</h3>
+          <span>{invoice.paymentDue}</span>
+          <span>{invoice.clientName}</span>
+          <span>&euro;{invoice.total}</span>
+          <div>{invoice.status}</div>
+        </a>
+      </li>
+    </ul>
+  );
+};
+
+export default InvoicesList;
--- src/components/pages/Invoices.tsx
+++ src/components/pages/Invoices.tsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
+import InvoicesList from "../invoices/InvoicesList";
+import { Invoice } from "../../types";
+
 const InvoicesPage = () => {
-  return <h1>Invoices Page</h1>;
+  const [invoices, setInvoices] = useState<Invoice[]>([]);
+
+  useEffect(() => {
+    const fetchInvoices = async () => {
+      try {
+        const response = await fetch(
+          "https://react-http-35b06-default-rtdb.firebaseio.com/.json"
+        );
+
+        if (!response.ok) {
+          throw new Error("Could not fetch the data.");
+        }
+
+        const resData = await response.json();
+        setInvoices(resData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchInvoices();
+  }, []);
+
+  return (
+    <>
+      <h1>Invoices</h1>
+      {invoices.map((invoice) => (
+        <InvoicesList key={invoice.id} invoice={invoice} />
+      ))}
+    </>
+  );
 };
 
 export default InvoicesPage;
