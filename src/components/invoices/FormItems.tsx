c65131a48d161c8f983d53b59211407bea636e0d
AgneBalc <agne.martinaityte@gmail.com>
2023-08-08 16:41:28 +0300

    useFormik changed to Formik component in InvoiceForm

--- src/components/invoices/FormItems.tsx
+++ src/components/invoices/FormItems.tsx
@@ -1,53 +1,74 @@
 import { Invoice, Item } from "../../types";
-import { FormikProps } from "formik";
+import { FieldArray, FormikProps } from "formik";
+import { ReactComponent as PlusIcon } from "../../assets/icon-plus.svg";
 
 interface FormItemsProps {
-  item: Item;
-  index: number;
+  items: Item[];
   formik: FormikProps<Invoice>;
 }
 
-const FormItems = ({ item, index, formik }: FormItemsProps) => {
-  console.log(item);
+const FormItems = ({ items, formik }: FormItemsProps) => {
   return (
     <div>
-      <div className="item-name">
-        <label htmlFor={`item-name-${index}`}>Item Name</label>
-        <input
-          type="text"
-          id={`item-name-${index}`}
-          {...formik.getFieldProps(`items[${index}].name`)}
-        />
-      </div>
-      {/* <div className="item-qty">
-        <label htmlFor="item-qty">Qty</label>
-        <input
-          type="number"
-          id="item-qty"
-          min="0"
-          step="1"
-          {...formik.getFieldProps("item-qty")}
-        />
-      </div>
-      <div className="item-price">
-        <label htmlFor="item-price">Price</label>
-        <input
-          type="number"
-          id="item-price"
-          min="0"
-          step="0.01"
-          {...formik.getFieldProps("item-price")}
-        />
-      </div>
-      <div className="item-total">
-        <label htmlFor="item-total">Total</label>
-        <input
-          type="number"
-          id="item-total"
-          readOnly={true}
-          {...formik.getFieldProps("item-total")}
-        />
-      </div> */}
+      <h3>Items List</h3>
+      <FieldArray
+        name="items"
+        render={(arrayHelpers) => (
+          <div>
+            {items?.length > 0 &&
+              items?.map((_, index: number) => (
+                <div key={index}>
+                  <div className="item-name">
+                    <label htmlFor={`item-name-${index}`}>Item Name</label>
+                    <input
+                      type="text"
+                      id={`item-name-${index}`}
+                      {...formik.getFieldProps(`items[${index}].name`)}
+                    />
+                  </div>
+                  <div className="item-qty">
+                    <label htmlFor={`item-quantity-${index}`}>Qty</label>
+                    <input
+                      type="number"
+                      id={`item-quantity-${index}`}
+                      min="0"
+                      step="1"
+                      {...formik.getFieldProps(`items[${index}].quantity`)}
+                    />
+                  </div>
+                  <div className="item-price">
+                    <label htmlFor={`item-price-${index}`}>Price</label>
+                    <input
+                      type="number"
+                      id={`item-price-${index}`}
+                      min="0"
+                      step="0.01"
+                      {...formik.getFieldProps(`items[${index}].price`)}
+                    />
+                  </div>
+                  <div className="item-total">
+                    <label htmlFor={`item-total-${index}`}>Total</label>
+                    <input
+                      type="number"
+                      id={`item-total-${index}`}
+                      readOnly={true}
+                      {...formik.getFieldProps(`items[${index}].total`)}
+                    />
+                  </div>
+                </div>
+              ))}
+            <button
+              type="button"
+              onClick={() =>
+                arrayHelpers.push({ name: "", quantity: 1, total: 0, price: 0 })
+              }
+            >
+              <PlusIcon />
+              <span>Add New Item</span>
+            </button>
+          </div>
+        )}
+      />
     </div>
   );
 };
--- src/components/invoices/InvoiceForm.tsx
+++ src/components/invoices/InvoiceForm.tsx
@@ -1,6 +1,5 @@
-import { useFormik } from "formik";
-import { ReactComponent as PlusIcon } from "../../assets/icon-plus.svg";
-import { Invoice, Item } from "../../types";
+import { Form, Formik } from "formik";
+import { Invoice } from "../../types";
 import {
   createId,
   getPaymentDueDate,
@@ -11,7 +10,6 @@ import { createInvoice } from "./invoicesApi";
 import { useNavigate } from "react-router-dom";
 import { format } from "date-fns";
 import FormItems from "./FormItems";
-import { useState } from "react";
 
 const initialValues: Invoice = {
   id: "",
@@ -39,31 +37,21 @@ const initialValues: Invoice = {
 };
 
 const InvoiceForm = () => {
-  const [itemsList, setItemsList] = useState<Item[]>([]);
-
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const { invoices } = useAppSelector((state) => state.invoices);
 
-  const handleAddItem = () => {
-    const newItem: Item = { name: "New Item", quantity: 1, total: 0, price: 0 };
-    setItemsList((prevItems) => prevItems.concat(newItem));
+  const handleSubmit = (values: Invoice) => {
+    dispatch(
+      createInvoice({
+        ...values,
+        id: createId(invoices),
+        paymentDue: getPaymentDueDate(values.createdAt, values.paymentTerms),
+        status: "pending",
+      })
+    );
+    navigate("/");
   };
-
-  const formik = useFormik({
-    initialValues,
-    onSubmit: (values) => {
-      dispatch(
-        createInvoice({
-          ...values,
-          id: createId(invoices),
-          paymentDue: getPaymentDueDate(values.createdAt, values.paymentTerms),
-          status: "pending",
-        })
-      );
-      navigate("/");
-    },
-  });
 
   const handleSaveAsDraft = (values: Invoice) => {
     console.log(values);
@@ -78,148 +66,155 @@ const InvoiceForm = () => {
   };
 
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <fieldset>
-        <legend>Bill From</legend>
-        <div>
-          <label htmlFor="senderAddress.street">Street Address</label>
-          <input
-            type="text"
-            id="senderAddress.street"
-            {...formik.getFieldProps("senderAddress.street")}
-          />
-        </div>
-        <div>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={(values) => {
+        handleSubmit(values);
+      }}
+    >
+      {(formik) => (
+        <Form>
+          <fieldset>
+            <legend>Bill From</legend>
+            <div>
+              <label htmlFor="senderAddress.street">Street Address</label>
+              <input
+                type="text"
+                id="senderAddress.street"
+                {...formik.getFieldProps("senderAddress.street")}
+              />
+            </div>
+            <div>
+              <div>
+                <label htmlFor="senderAddress.city">City</label>
+                <input
+                  type="text"
+                  id="senderAddress.city"
+                  {...formik.getFieldProps("senderAddress.city")}
+                />
+              </div>
+              <div>
+                <label htmlFor="senderAddress.postCode">Postal Code</label>
+                <input
+                  type="text"
+                  id="senderAddress.postCode"
+                  {...formik.getFieldProps("senderAddress.postCode")}
+                />
+              </div>
+              <div>
+                <label htmlFor="senderAddress.country">Country</label>
+                <input
+                  type="text"
+                  id="senderAddress.country"
+                  {...formik.getFieldProps("senderAddress.country")}
+                />
+              </div>
+            </div>
+          </fieldset>
+          <fieldset>
+            <legend>Bill To</legend>
+            <div>
+              <label htmlFor="clientName">Client's Name</label>
+              <input
+                type="text"
+                id="clientName"
+                {...formik.getFieldProps("clientName")}
+              />
+            </div>
+            <div>
+              <label htmlFor="clientEmail">Client's Email</label>
+              <input
+                type="email"
+                id="clientEmail"
+                placeholder="e.g. email@example.com"
+                {...formik.getFieldProps("clientEmail")}
+              />
+            </div>
+            <div>
+              <label htmlFor="clientAddress.street">Street Address</label>
+              <input
+                type="text"
+                id="clientAddress.street"
+                {...formik.getFieldProps("clientAddress.street")}
+              />
+            </div>
+            <div>
+              <div>
+                <label htmlFor="clientAddress.city">City</label>
+                <input
+                  type="text"
+                  id="clientAddress.city"
+                  {...formik.getFieldProps("clientAddress.city")}
+                />
+              </div>
+              <div>
+                <label htmlFor="clientAddress.postCode">Postal Code</label>
+                <input
+                  type="text"
+                  id="clientAddress.postCode"
+                  {...formik.getFieldProps("clientAddress.postCode")}
+                />
+              </div>
+              <div>
+                <label htmlFor="clientAddress.country">Country</label>
+                <input
+                  type="text"
+                  id="clientAddress.country"
+                  {...formik.getFieldProps("clientAddress.country")}
+                />
+              </div>
+            </div>
+          </fieldset>
           <div>
-            <label htmlFor="senderAddress.city">City</label>
-            <input
-              type="text"
-              id="senderAddress.city"
-              {...formik.getFieldProps("senderAddress.city")}
-            />
+            <div>
+              <div>
+                <label htmlFor="createdAt">Invoice Date</label>
+                <input
+                  type="date"
+                  id="createdAt"
+                  {...formik.getFieldProps("createdAt")}
+                />
+              </div>
+              <div>
+                <label htmlFor="paymentTerms">Payment Terms</label>
+                <select
+                  id="paymentTerms"
+                  {...formik.getFieldProps("paymentTerms")}
+                >
+                  {paymentTermsOptions.map((option) => (
+                    <option value={option.value} key={option.value}>
+                      {option.text}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+            <div>
+              <label htmlFor="description">Project Description</label>
+              <input
+                type="text"
+                id="description"
+                placeholder="e.g. Graphic Design Service"
+                {...formik.getFieldProps("description")}
+              />
+            </div>
           </div>
           <div>
-            <label htmlFor="senderAddress.postCode">Postal Code</label>
-            <input
-              type="text"
-              id="senderAddress.postCode"
-              {...formik.getFieldProps("senderAddress.postCode")}
-            />
+            <FormItems items={formik.values.items} formik={formik} />
           </div>
-          <div>
-            <label htmlFor="senderAddress.country">Country</label>
-            <input
-              type="text"
-              id="senderAddress.country"
-              {...formik.getFieldProps("senderAddress.country")}
-            />
+          <div className="form-buttons">
+            <button type="button">Discard</button>
+            <button
+              type="button"
+              onClick={() => handleSaveAsDraft(formik.values)}
+            >
+              Save as Draft
+            </button>
+            <button type="submit">Save & Send</button>
           </div>
-        </div>
-      </fieldset>
-      <fieldset>
-        <legend>Bill To</legend>
-        <div>
-          <label htmlFor="clientName">Client's Name</label>
-          <input
-            type="text"
-            id="clientName"
-            {...formik.getFieldProps("clientName")}
-          />
-        </div>
-        <div>
-          <label htmlFor="clientEmail">Client's Email</label>
-          <input
-            type="email"
-            id="clientEmail"
-            placeholder="e.g. email@example.com"
-            {...formik.getFieldProps("clientEmail")}
-          />
-        </div>
-        <div>
-          <label htmlFor="clientAddress.street">Street Address</label>
-          <input
-            type="text"
-            id="clientAddress.street"
-            {...formik.getFieldProps("clientAddress.street")}
-          />
-        </div>
-        <div>
-          <div>
-            <label htmlFor="clientAddress.city">City</label>
-            <input
-              type="text"
-              id="clientAddress.city"
-              {...formik.getFieldProps("clientAddress.city")}
-            />
-          </div>
-          <div>
-            <label htmlFor="clientAddress.postCode">Postal Code</label>
-            <input
-              type="text"
-              id="clientAddress.postCode"
-              {...formik.getFieldProps("clientAddress.postCode")}
-            />
-          </div>
-          <div>
-            <label htmlFor="clientAddress.country">Country</label>
-            <input
-              type="text"
-              id="clientAddress.country"
-              {...formik.getFieldProps("clientAddress.country")}
-            />
-          </div>
-        </div>
-      </fieldset>
-      <div>
-        <div>
-          <div>
-            <label htmlFor="createdAt">Invoice Date</label>
-            <input
-              type="date"
-              id="createdAt"
-              {...formik.getFieldProps("createdAt")}
-            />
-          </div>
-          <div>
-            <label htmlFor="paymentTerms">Payment Terms</label>
-            <select id="paymentTerms" {...formik.getFieldProps("paymentTerms")}>
-              {paymentTermsOptions.map((option) => (
-                <option value={option.value} key={option.value}>
-                  {option.text}
-                </option>
-              ))}
-            </select>
-          </div>
-        </div>
-        <div>
-          <label htmlFor="description">Project Description</label>
-          <input
-            type="text"
-            id="description"
-            placeholder="e.g. Graphic Design Service"
-            {...formik.getFieldProps("description")}
-          />
-        </div>
-      </div>
-      <div>
-        <h3>Items List</h3>
-        {itemsList.length > 0 &&
-          itemsList.map((item, index) => (
-            <FormItems key={index} item={item} index={index} formik={formik} />
-          ))}
-        <button type="button" onClick={() => handleAddItem()}>
-          <PlusIcon />
-          <span>Add New Item</span>
-        </button>
-      </div>
-      <div className="form-buttons">
-        <button type="button">Discard</button>
-        <button type="button" onClick={() => handleSaveAsDraft(formik.values)}>
-          Save as Draft
-        </button>
-        <button type="submit">Save & Send</button>
-      </div>
-    </form>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
