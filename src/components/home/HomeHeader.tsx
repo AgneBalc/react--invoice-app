b5d16d24102711d17091de14fc6fb9677c8e6883
AgneBalc <agne.martinaityte@gmail.com>
2023-08-23 10:44:27 +0300

    HomeHeader componnet created

---
+++ src/components/home/HomeHeader.tsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as IconPlus } from "../../assets/icon-plus.svg";
+import { Invoice, InvoiceStatus } from "../../types";
+import { ReactComponent as IconDown } from "../../assets/icon-arrow-down.svg";
+
+interface HomeHeaderProps {
+  invoices: Invoice[];
+  selectedStatus: string;
+  setSelectedStatus: (status: string) => void;
+}
+
+const HomeHeader = ({
+  invoices,
+  selectedStatus,
+  setSelectedStatus,
+}: HomeHeaderProps) => {
+  const [showFilter, setShowFilter] = useState<boolean>(false);
+
+  const filters: InvoiceStatus[] = ["paid", "pending", "draft"];
+
+  const handleFilterToggle = () => {
+    setShowFilter(!showFilter);
+  };
+
+  const handleFilter = (status: string) => {
+    setSelectedStatus(status === selectedStatus ? "" : status);
+    setShowFilter(false);
+  };
+
+  return (
+    <div className="heading">
+      <div>
+        <h1>Invoices</h1>
+        {invoices.length ? (
+          <p>{invoices.length} Invoices</p>
+        ) : (
+          <p>No invoices</p>
+        )}
+      </div>
+      <div>
+        <div onClick={handleFilterToggle}>
+          Filter by status
+          <IconDown />
+        </div>
+        {showFilter &&
+          filters.map((filter, index) => (
+            <label htmlFor={filter} key={index}>
+              <input
+                type="checkbox"
+                name="status"
+                id={filter}
+                value={filter}
+                checked={selectedStatus === filter}
+                onChange={() => handleFilter(filter)}
+              />
+              {filter}
+            </label>
+          ))}
+      </div>
+      <button>
+        <Link to="new">
+          <IconPlus />
+          <span>New Invoice</span>
+        </Link>
+      </button>
+    </div>
+  );
+};
+
+export default HomeHeader;
--- src/pages/Invoices.tsx
+++ src/pages/Invoices.tsx
@@ -1,24 +1,12 @@
 import InvoicesList from "../components/home/InvoicesList";
-import { ReactComponent as IconPlus } from "../assets/icon-plus.svg";
 import { useAppSelector } from "../hooks/redux-hooks";
-import { Link } from "react-router-dom";
 import { useState } from "react";
 import CreateInvoiceMessage from "../components/home/CreateInvoiceMessage";
-import FilterDropdown from "../components/home/FilterDropdown";
+import HomeHeader from "../components/home/HomeHeader";
 
 const InvoicesPage = () => {
   const [selectedStatus, setSelectedStatus] = useState("");
-  const [showFilter, setShowFilter] = useState(false);
   const { invoices } = useAppSelector((state) => state.invoices);
-
-  const handleFilterToggle = () => {
-    setShowFilter(!showFilter);
-  };
-
-  const handleFilter = (status: string) => {
-    setSelectedStatus(status === selectedStatus ? "" : status);
-    setShowFilter(false);
-  };
 
   const filteredInvoices =
     selectedStatus === ""
@@ -27,26 +15,11 @@ const InvoicesPage = () => {
 
   return (
     <>
-      <div className="heading">
-        <h1>Invoices</h1>
-        {filteredInvoices.length ? (
-          <p>{filteredInvoices.length} Invoices</p>
-        ) : (
-          <p>No invoices</p>
-        )}
-        <FilterDropdown
-          handleFilter={handleFilter}
-          selectedStatus={selectedStatus}
-          handleFilterToggle={handleFilterToggle}
-          showFilter={showFilter}
-        />
-        <button>
-          <Link to="new">
-            <IconPlus />
-            <span>New Invoice</span>
-          </Link>
-        </button>
-      </div>
+      <HomeHeader
+        invoices={filteredInvoices}
+        selectedStatus={selectedStatus}
+        setSelectedStatus={setSelectedStatus}
+      />
       {!filteredInvoices.length ? (
         <CreateInvoiceMessage />
       ) : (
